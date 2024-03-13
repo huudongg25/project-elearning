4ec344d04ab6555de2281d32804197aa0a88234e
milotriss <nhattienla98@gmail.com>
2024-03-13 09:30:50 +0700

    config db-config

--- elearning-server/src/configs/db.config.ts
+++ elearning-server/src/configs/db.config.ts
@@ -3,7 +3,7 @@ import {config} from 'dotenv';
 import { Sequelize } from 'sequelize';
 
 config();
-const sequelize = new Sequelize(String(process.env.DB_NAME),String(process.env.DB_USER),String(process.env.DB_PASS),{
+const sequelize = new Sequelize("project_elearning","root",String(process.env.DB_PASS),{
     host: process.env.DB_HOST,
     dialect: 'mysql'
 })
