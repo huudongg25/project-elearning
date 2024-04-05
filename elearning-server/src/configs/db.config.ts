413bc364e2418fde7ea4c8575328a895f56cd715
huudongg25 <thdong98225@gmail.com>
2024-04-05 09:50:40 +0700

    update

--- elearning-server/src/configs/db.config.ts
+++ elearning-server/src/configs/db.config.ts
@@ -1,10 +1,10 @@
 import mysql2 from 'mysql2';
-import {config} from 'dotenv';
+import { config } from 'dotenv';
 import { Sequelize } from 'sequelize';
 
 config();
-const sequelize = new Sequelize("project_elearning","root",String(process.env.DB_PASS),{
-    host: process.env.DB_HOST,
+const sequelize = new Sequelize("bjnygsiqaed6ixdojjh9", "u1sjvhxmdiq1u6uw", "PtMjPkpb53Qn4AnMW86U", {
+    host: "bjnygsiqaed6ixdojjh9-mysql.services.clever-cloud.com",
     dialect: 'mysql'
 })
 
